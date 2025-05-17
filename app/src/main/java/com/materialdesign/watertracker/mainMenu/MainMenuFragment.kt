332af0ec2eda3f6cecb7973c9ddb811ba36e0b5f
h4sanzade <h4sanzade@gmail.com>
2025-05-17 04:10:29 +0400

    update

--- app/src/main/java/com/materialdesign/watertracker/mainMenu/MainMenuFragment.kt
+++ app/src/main/java/com/materialdesign/watertracker/mainMenu/MainMenuFragment.kt
@@ -1,5 +1,7 @@
 package com.materialdesign.watertracker.mainMenu
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +11,7 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.materialdesign.watertracker.PreferenceHelper
 import com.materialdesign.watertracker.R
 import com.materialdesign.watertracker.databinding.FragmentMainMenuBinding
@@ -29,6 +32,7 @@ class MainMenuFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Kullanıcı adı kontrolü
         val username = PreferenceHelper.getUsername(requireContext())
 
         if (username == null) {
@@ -39,6 +43,24 @@ class MainMenuFragment : Fragment() {
             binding.greetingPrefix.text = parts[0]
             binding.greetingName.text = parts[1].trim()
         }
+
+        // Icon rengi mavi, text sabit gri
+        val states = arrayOf(
+            intArrayOf(android.R.attr.state_checked),
+            intArrayOf(-android.R.attr.state_checked)
+        )
+        val iconColors = intArrayOf(
+            Color.parseColor("#08ABFF"), // selected icon
+            Color.parseColor("#808080")  // unselected icon
+        )
+        val colorStateList = ColorStateList(states, iconColors)
+
+        // Eğer BottomNavigationView fragment içinde ise
+        binding.bottomNavigation.itemIconTintList = colorStateList
+
+        // Text rengi sabit gri
+        val textColor = Color.parseColor("#808080")
+        binding.bottomNavigation.setItemTextColor(ColorStateList.valueOf(textColor))
     }
 
     private fun showNameInputDialog() {
@@ -61,8 +83,6 @@ class MainMenuFragment : Fragment() {
 
             if (firstName.isNotEmpty() && lastName.isNotEmpty()) {
                 val fullName = "$firstName $lastName"
-
-
                 PreferenceHelper.saveUsername(requireContext(), fullName)
 
                 Toast.makeText(requireContext(), "Name saved!", Toast.LENGTH_SHORT).show()
